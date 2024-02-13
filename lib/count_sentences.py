79079990b7342798ffc6cf8e64ea205dadcf2245
Tony Eder <tonyeder11@gmail.com>
2024-02-13 13:59:31 -0800

    on to the last one finally

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        "Retrieving value of self"
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+          print("The value must be a string.")
+        else:
+          self._value = value      
+          
+    def is_sentence(self):
+        return True if self.value.endswith(".") else False
+
+    def is_question(self):
+        return True if self.value.endswith("?") else False
+
+    def is_exclamation(self):
+        return True if self.value.endswith("!") else False
+
+    def count_sentences(self):
+        pass
+          
+
+
+# print 'The value must be a string.' if determined value is not a string.
+# to determine if its a string I want to use type(value) == str
+# returns True if value ends with a period and False otherwise.
+# returns True if value ends with a question mark and False otherwise.
+# returns True if value ends with an exclamation mark and False otherwise.
+# returns the number of sentences in the value.
+
+        
